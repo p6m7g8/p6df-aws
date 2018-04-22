c1df5ce74ce46889fec0590736a23e05d001e32e
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-04-22 00:01:20 -0400

    brew

--- init.zsh
+++ init.zsh
@@ -2,6 +2,12 @@ p6df::modules::aws::version() { echo "0.0.1" }
 p6df::modules::aws::deps() { 
 	ModuleDeps=()
 }
+
+# aws
+# awsdocs
+# awslabs
+# aws-samples
+# awscli
  
 p6df::modules::aws::externali::brew() { 
 
