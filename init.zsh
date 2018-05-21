71f13a92677c11ce95465595b05ce4a3483df5ff
Philip M. Gollucci <pgollucci@p6m7g8.com>
2018-05-21 12:27:41 -0400

    sync

--- init.zsh
+++ init.zsh
@@ -1,6 +1,6 @@
 p6df::modules::aws::version() { echo "0.0.1" }
 p6df::modules::aws::deps() { 
-	ModuleDeps=()
+	ModuleDeps=(p6m7g8/p6aws)
 }
 
 # aws
@@ -9,6 +9,8 @@ p6df::modules::aws::deps() {
 # aws-samples
 # awscli
  
+#  source <(awless completion zsh)
+
 p6df::modules::aws::externali::brew() { 
 
   brew tap wallix/awless
@@ -20,7 +22,51 @@ p6df::modules::aws::externali::brew() {
 
 p6df::modules::aws::init() {
 
-  source <(awless completion zsh)
+}
+
+p6df::prompt::aws::line() {
+
+  aws_sts_target_source_prompt_info
+  aws_sts_source_prompt_info
+  aws_sts_prompt_info
+}
+
+aws_sts_target_source_prompt_info() {
+
+  local assumed=
+  if [ -n "$AWS_SOURCE_DEFAULT_PROFILE" ]; then
+      assumed="ASSUMED:"
+  fi
+  if [ -n "$AWS_DEFAULT_PROFILE" ]; then
+    echo "aws:\t${assumed}[$AWS_DEFAULT_PROFILE($AWS_ENV/$ENV_LEVEL) - $AWS_DEFAULT_REGION($AWS_VPC)]"
+  fi
+}
+
+aws_sts_source_prompt_info() {
+
+  if [ -n "$AWS_SOURCE_DEFAULT_PROFILE" ]; then
+    echo "aws:\tSOURCE:[$AWS_SOURCE_DEFAULT_PROFILE($AWS_SOURCE_ENV/$SOURCE_ENV_LEVEL) - $AWS_SOURCE_DEFAULT_REGION($AWS_VPC)]"
+  fi
+}
+
+aws_sts_prompt_info() {
+
+    local creds=~/.aws/credentials
+    [ -e $creds ] || return
+
+    local mtime=$(mtime "$creds")
+    local now=$(now)
+    local diff=$(($now-$mtime))
+
+    if [ $diff -gt 7200 ]; then
+        echo ""
+    elif [ $diff -gt 3600 ]; then
+        echo "sts:\t${red}$diff${norm}s"
+    elif [ $diff -gt 3500 ]; then
+        echo "sts:\t${cyan}$diff${norm}s"
+    else
+        echo "sts:\t${green}$diff${norm}s"
+    fi
 }
 
 p6df::modules::aws::init
