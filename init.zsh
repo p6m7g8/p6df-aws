6de30f64b3e20ba7e09846b0cddd3c555fc60a53
Philip M. Gollucci <pgollucci@p6m7g8.com>
2019-05-12 12:45:42 -0400

    symlink, more brews, indent

--- init.zsh
+++ init.zsh
@@ -1,7 +1,5 @@
 p6df::modules::aws::version() { echo "0.0.1" }
-p6df::modules::aws::deps() {
-	ModuleDeps=(p6m7g8/p6aws)
-}
+p6df::modules::aws::deps() { ModuleDeps=(p6m7g8/p6aws) }
 
 # aws
 # awsdocs
@@ -16,33 +14,37 @@ p6df::modules::aws::external::brew() {
   brew install awless
 
   brew install awscli
+  brew install aws-iam-authenticator
+
+  brew tap aws/tap
+  brew install aws-sam-cli
+
   brew install aws-shell
 }
 
 p6df::modules::aws::home::symlink() {
 
- # XXX: symlink to private
- true;
+  ln -fs $P6_DFZ_SRC_DIR/$USER/home-private/aws .aws
 }
 
 p6df::modules::aws::init() {
 
-    AWS_DIR=$HOME/.aws
-    AWS_ORG=$P6_AWS_ORG
-    
-    AWS_ACCOUNT_MAP=$AWS_DIR/map-$AWS_ORG
-    AWS_CREDENTIAL_FILE=$AWS_DIR/credentials
-    AWS_ASSUMED_CREDENTIAL_FILE=$AWS_CREDENTIAL_FILE-assumed
-    AWS_SOURCE_CREDENTIAL_FILE=$AWS_CREDENTIAL_FILE-source
+  AWS_DIR=$HOME/.aws
+  AWS_ORG=$P6_AWS_ORG
 
-    AWS_ROLE_SESSION_NAME=$DAAS_JC_EMAIL
+  AWS_ACCOUNT_MAP=$AWS_DIR/map-$AWS_ORG
+  AWS_CREDENTIAL_FILE=$AWS_DIR/credentials
+  AWS_ASSUMED_CREDENTIAL_FILE=$AWS_CREDENTIAL_FILE-assumed
+  AWS_SOURCE_CREDENTIAL_FILE=$AWS_CREDENTIAL_FILE-source
 
-    alias sts="p6_GLOBAL_aws_sts_svc_refresh"
-    alias ac="p6_GLOBAL_aws_organizations_svc_account_make"
-    alias asc="p6_GLOBAL_aws_shortcuts"
-    alias aosu="p6_GLOBAL_aws_sts_svc_org_su"
-    alias asu="p6_GLOBAL_aws_sts_svc_role_assume"
-    alias assh="p6_GLOBAL_aws_ssh_svc_do"
+  AWS_ROLE_SESSION_NAME=$DAAS_JC_EMAIL
+
+  alias sts="p6_GLOBAL_aws_sts_svc_refresh"
+  alias ac="p6_GLOBAL_aws_organizations_svc_account_make"
+  alias asc="p6_GLOBAL_aws_shortcuts"
+  alias aosu="p6_GLOBAL_aws_sts_svc_org_su"
+  alias asu="p6_GLOBAL_aws_sts_svc_role_assume"
+  alias assh="p6_GLOBAL_aws_ssh_svc_do"
 }
 
 p6df::prompt::aws::line() {
@@ -53,20 +55,21 @@ p6df::prompt::aws::line() {
 }
 
 p6_GLOBAL_aws_shortcuts() {
-    p6_aws_shortcuts "$AWS_ORG" "$AWS_CREDENTIAL_FILE"
+  p6_aws_shortcuts "$AWS_ORG" "$AWS_CREDENTIAL_FILE"
 }
 
 p6_GLOBAL_aws_organizations_svc_account_make() {
-    local account_alias="$1"
+  local account_alias="$1"
 
-    local email="pgollucci+aws+$account_alias@p6m7g8.com"
+  # XXX: move to zsh-me
+  local email="pgollucci+aws+$account_alias@p6m7g8.com"
 
-    p6_aws_organizations_svc_account_make "$AWS_CREDENTIAL_FILE" "$AWS_SOURCE_CREDENTIAL_FILE" "$AWS_ASSUMED_CREDENTIAL_FILE" "$AWS_ORG" \
-					  "$account_alias" "$email" "$AWS_ACCOUNT_MAP" "JumpCloud" "$DAAS_JC_EMAIL" \
-					  "us-east-1" "text" \
-					  "/SSO/SSO_Admin" "arn:aws:iam::aws:policy/AdministratorAccess" \
-					  "/C=US/ST=MD/L=Upper_Marlboro/O=$AWS_ORG/OU=Technology/CN=$account_alias" \
-					  "4096" "1024"
+  p6_aws_organizations_svc_account_make "$AWS_CREDENTIAL_FILE" "$AWS_SOURCE_CREDENTIAL_FILE" "$AWS_ASSUMED_CREDENTIAL_FILE" "$AWS_ORG" \
+					"$account_alias" "$email" "$AWS_ACCOUNT_MAP" "JumpCloud" "$DAAS_JC_EMAIL" \
+					"us-east-1" "text" \
+					"/SSO/SSO_Admin" "arn:aws:iam::aws:policy/AdministratorAccess" \
+					"/C=US/ST=MD/L=Upper_Marlboro/O=$AWS_ORG/OU=Technology/CN=$account_alias" \
+					"4096" "1024"
 }
 
 p6_GLOBAL_aws_sts_svc_refresh() {
@@ -110,21 +113,21 @@ p6_aws_sts_source_prompt_info() {
 }
 
 p6_aws_sts_prompt_info() {
-    local creds="$1"
+  local creds="$1"
 
-    p6_file_exists $creds || return
+  p6_file_exists $creds || return
 
-    local mtime=$(p6_dt_mtime "$creds")
-    local now=$(p6_dt_now_epoch_seconds)
-    local diff=$(($now-$mtime))
+  local mtime=$(p6_dt_mtime "$creds")
+  local now=$(p6_dt_now_epoch_seconds)
+  local diff=$(($now-$mtime))
 
-    if [ $diff -gt 7200 ]; then
-	echo ""
-    elif [ $diff -gt 3600 ]; then
-	echo "sts:\t$diff"
-    elif [ $diff -gt 3500 ]; then
-	echo "sts:\t$diff"
-    else
-	echo "sts:\t$diff"
-    fi
+  if [ $diff -gt 7200 ]; then
+    echo ""
+  elif [ $diff -gt 3600 ]; then
+    echo "sts:\t$diff"
+  elif [ $diff -gt 3500 ]; then
+    echo "sts:\t$diff"
+  else
+    echo "sts:\t$diff"
+  fi
 }
