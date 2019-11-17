cd8bd24f8efdbe822c6a0903a0e4d5d42088d677
Philip M. Gollucci <pgollucci@p6m7g8.com>
2019-11-17 18:03:43 -0500

    remove globals et al, new sts prompt

--- init.zsh
+++ init.zsh
@@ -57,17 +57,6 @@ p6df::modules::aws::home::symlink() {
 
 p6df::modules::aws::init() {
 
-  AWS_DIR=$HOME/.aws
-  AWS_ORG=$P6_AWS_ORG
-
-  AWS_ACCOUNT_MAP=$AWS_DIR/map-$AWS_ORG
-  AWS_CREDENTIAL_FILE=$AWS_DIR/credentials
-  AWS_ASSUMED_CREDENTIAL_FILE=$AWS_CREDENTIAL_FILE-assumed
-  AWS_SOURCE_CREDENTIAL_FILE=$AWS_CREDENTIAL_FILE-source
-
-  AWS_ROLE_SESSION_NAME=$DAAS_JC_EMAIL
-
-  p6df::modules::aws::p6aliases
   p6df::modules::aws::cdkaliases
 }
 
@@ -84,102 +73,11 @@ p6df::modules::aws::cdkaliases() {
   alias lw='lr watch'
 }
 
-p6df::modules::aws::p6aliases() {
-
-  alias sts="p6_GLOBAL_aws_sts_svc_refresh"
-  alias ac="p6_GLOBAL_aws_organizations_svc_account_make"
-  alias asc="p6_GLOBAL_aws_shortcuts"
-  alias aosu="p6_GLOBAL_aws_sts_svc_org_su"
-  alias asu="p6_GLOBAL_aws_sts_svc_role_assume"
-  alias assh="p6_GLOBAL_aws_ssh_svc_do"
-}
-
 p6df::prompt::aws::line() {
 
-  p6_aws_sts_target_source_prompt_info
-  p6_aws_sts_source_prompt_info
-  p6_aws_sts_prompt_info "$AWS_CREDENTIAL_FILE"
-}
+  p6_aws_cfg_prompt_info "_active"
+  p6_aws_cfg_prompt_info "_source"
+  p6_aws_cfg_prompt_info "_saved"
 
-p6_GLOBAL_aws_shortcuts() {
-  p6_aws_shortcuts "$AWS_ORG" "$AWS_CREDENTIAL_FILE"
-}
-
-p6_GLOBAL_aws_organizations_svc_account_make() {
-  local account_alias="$1"
-
-  # XXX: move to zsh-me
-  local email="pgollucci+aws+$account_alias@p6m7g8.com"
-
-  p6_aws_organizations_svc_account_make "$AWS_CREDENTIAL_FILE" "$AWS_SOURCE_CREDENTIAL_FILE" "$AWS_ASSUMED_CREDENTIAL_FILE" "$AWS_ORG" \
-					"$account_alias" "$email" "$AWS_ACCOUNT_MAP" "JumpCloud" "$DAAS_JC_EMAIL" \
-					"us-east-1" "text" \
-					"/SSO/SSO_Admin" "arn:aws:iam::aws:policy/AdministratorAccess" \
-					"/C=US/ST=MD/L=Upper_Marlboro/O=$AWS_ORG/OU=Technology/CN=$account_alias" \
-					"4096" "1024"
-}
-
-p6_GLOBAL_aws_sts_svc_refresh() {
-
-  p6_aws_sts_svc_refresh "$AWS_CREDENTIAL_FILE" "$AWS_ACCOUNT_MAP" "$AWS_ORG" "$DAAS_JC_EMAIL"
-}
-
-p6_GLOBAL_aws_sts_svc_org_su() {
-  local org="$1"
-
-  p6_aws_organizations_svc_su "$org" "$AWS_ACCOUNT_MAP" "us-east-1" "text" "OrganizationAccountAccessRole" "$AWS_ROLE_SESSION_NAME" "$AWS_CREDENTIAL_FILE" "$AWS_SOURCE_CREDENTIAL_FILE" "$AWS_ASSUMED_CREDENTIAL_FILE"
-}
-
-p6_GLOBAL_aws_sts_svc_org_su_un() {
-
-  p6_aws_organizations_svc_su_un "$AWS_CREDENTIAL_FILE" "$AWS_SOURCE_CREDENTIAL_FILE" "$AWS_ASSUMED_CREDENTIAL_FILE"
-}
-
-p6_GLOBAL_aws_sts_svc_role_assume() {
-
-  p6_aws_sts_svc_role_assume "$2" "us-east-1" "text" "$1" $AWS_ROLE_SESSION_NAME $AWS_CREDENTIAL_FILE $AWS_SOURCE_CREDENTIAL_FILE $AWS_ASSUMED_CREDENTIAL_FILE
-}
-
-p6_GLOBAL_aws_ssh_svc_do() {
- local host="$1"
-
- p6_aws_ssh_svc_do "${host}.p6m7g8.net"
-}
-
-p6_aws_sts_target_source_prompt_info() {
-
-  local assumed=
-  if [ -n "$AWS_SOURCE_DEFAULT_PROFILE" ]; then
-      assumed="ASSUMED:"
-  fi
-  if [ -n "$AWS_DEFAULT_PROFILE" ]; then
-    echo "aws:\t${assumed}[$AWS_DEFAULT_PROFILE($AWS_ENV/$ENV_LEVEL) - $AWS_DEFAULT_REGION($AWS_VPC_ID)]"
-  fi
-}
-
-p6_aws_sts_source_prompt_info() {
-
-  if [ -n "$AWS_SOURCE_DEFAULT_PROFILE" ]; then
-    echo "aws:\tSOURCE:[$AWS_SOURCE_DEFAULT_PROFILE($AWS_SOURCE_ENV/$SOURCE_ENV_LEVEL) - $AWS_SOURCE_DEFAULT_REGION($AWS_VPC)]"
-  fi
-}
-
-p6_aws_sts_prompt_info() {
-  local creds="$1"
-
-  p6_file_exists $creds || return
-
-  local mtime=$(p6_dt_mtime "$creds")
-  local now=$(p6_dt_now_epoch_seconds)
-  local diff=$(($now-$mtime))
-
-  if [ $diff -gt 7200 ]; then
-    echo ""
-  elif [ $diff -gt 3600 ]; then
-    echo "sts:\t$diff"
-  elif [ $diff -gt 3500 ]; then
-    echo "sts:\t$diff"
-  else
-    echo "sts:\t$diff"
-  fi
+  p6_aws_sts_prompt_info "$(p6_aws_cfg_env_shared_credentials_file_active)"
 }
