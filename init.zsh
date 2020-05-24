a83f7de6567e565a5e17c6d3c01222e250ba6e1e
Philip M. Gollucci <pgollucci@p6m7g8.com>
2020-05-23 23:32:15 -0400

    chore(prompt): standardize on _prompt_info (#2)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- init.zsh
+++ init.zsh
@@ -84,6 +84,11 @@ p6df::modules::aws::cdkaliases() {
 
 p6df::prompt::aws::line() {
 
+  p6_aws_prompt_info
+}
+
+p6_aws_prompt_info() {
+
   p6_aws_cdk_prompt_info
 
   p6_aws_cfg_prompt_info "_active"
